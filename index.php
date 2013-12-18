064304d273a0d7defd8e88f69a5c9a1ebe070364
Kaspar Naaber <kaspar@fraktal.ee>
2013-12-18 10:53:56 +0200

    Add front page content and style

--- index.php
+++ index.php
@@ -10,9 +10,13 @@
 
     <main class="page-content cfx" role="main">
       <header class="content-header">
-        Title goes here
+        <h1>Site title</h1>
+        <h2>Here’s a good place for your slogan.</h2>
       </header>
-      Content area
+      <h3>A better way to create a website</h3>
+      <p>Introduce your company (or yourself) here. Click around, try out how to edit text, choose a different design theme, upload pictures etc. This site is fully customizable — you can add, delete, modify any text or pictures as you desire.</p>
+      <p>Introduce your company (or yourself) here. Click around, try out how to edit text, choose a different design theme, upload pictures etc. This site is fully customizable — you can add, delete, modify any text or pictures as you desire.</p>
+      <p>Introduce your company (or yourself) here. Click around, try out how to edit text, choose a different design theme, upload pictures etc. This site is fully customizable — you can add, delete, modify any text or pictures as you desire.</p>
     </main>
 
     <?php require("components/site-footer.php") ?>
