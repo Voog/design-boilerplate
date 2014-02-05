9a8f2aab3e22717e2e750380d5eafd8fe3de7a7d
Kaspar Naaber <kaspar@fraktal.ee>
2014-02-05 15:30:45 +0200

    Remove unnecessary javascript functions

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,40 +1,26 @@
 ;(function($) {
-  // Adds class 'active' to main menu current element (Delete this function if the Edicy generated menu has been set up).
-  var hightlightActivePage = function() {
-    var url = window.location.href;
-    $('.menu a').filter(function() {
-      return this.href == url;
-    }).parent().addClass('active');
-  };
-
-  // Shows and hides specific elements on common, blog and article pages (Use liquid instead).
-  var commonPageSettings = function() {
-    $('.header-banner').hide();
+  // Toggle mobile menu
+  var menuToggle = function() {
+    $('.js-menu-toggle').click(function() {
+      $(this).next().toggleClass('expanded');
+    });
   };
 
   // Initiations
-  var initFrontPage = function() {
-    $('.header-left').hide();
-  };
-
   var initCommonPage = function() {
-    commonPageSettings();
   };
 
   var initBlogPage = function() {
-    commonPageSettings();
   };
 
   var initArticlePage = function() {
-    commonPageSettings();
   };
 
   var init = function() {
-    hightlightActivePage();
+    menuToggle();
   };
 
   window.project = {
-    initFrontPage: initFrontPage,
     initCommonPage: initCommonPage,
     initBlogPage: initBlogPage,
     initArticlePage: initArticlePage
