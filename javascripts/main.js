01825c1389ad9a689fffce8c2c2ed4045dc80ead
kaspar-naaber <kaspar.naaber@outlook.com>
2014-04-25 14:06:05 +0300

    Add comments to the main javascript functions

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,97 +1,117 @@
 ;(function($) {
-    var toggleMainMenu = function() {
-        $('.js-menu-btn').click(function() {
-            $(this).toggleClass('open');
-            $('.js-main-menu').toggleClass('expanded');
-        });
-    };
+  // SWITCHES THE SITE LANGUAGE TO THE SELECTED VALUE FROM THE LANGUAGE MENU
+  var handleLanguageSwitch = function() {
+    $('.menu-lang').find('.menu').change(function() {
+      console.log('changed');
+      window.location = $(this).find(':selected').val();
+    });
+  };
 
-    var toggleLangMenu = function() {
-        $('.js-lang-menu-btn').click(function(event) {
-            event.stopPropagation();
-            $('.js-lang-menu-popover').toggleClass('expanded');
-        });
-    };
+  // SHOWS/HIDES THE POPOVER MAIN MENU (VISIBLE ON SMALLES SCREENS)
+  var toggleMainMenu = function() {
+    $('.js-menu-btn').click(function() {
+      $(this).toggleClass('open');
+      $('.js-main-menu').toggleClass('expanded');
+    });
+  };
 
-    var handlePopoverMenuHide = function() {
-        $('html').click(function() {
-            if ($('.js-lang-menu-popover').hasClass('expanded')) {
-                $('.js-lang-menu-popover').removeClass('expanded');
-            }
-        });
-    };
+  // HIDES THE POPOVER MAIN MENU IF CICKED ANYWHERE ELSE THAN THE MENU ITSELF (VISIBLE ON SMALLES SCREENS)
+  var handlePopoverMenuHide = function() {
+    $('html').click(function() {
+      if ($('.js-lang-menu-popover').hasClass('expanded')) {
+        $('.js-lang-menu-popover').removeClass('expanded');
+      }
+    });
+  };
 
-    var handleFormFieldClick = function() {
-        $('.form_field_with_errors').click(function() {
-            $(this).removeClass('form_field_with_errors');
-        });
-    };
+  // HIDES THE FORM FIELD ERROR WHEN THE FIELD IS FOCUSED
+  var handleFormFieldClick = function() {
+    $('.form_field_with_errors').click(function() {
+      $(this).removeClass('form_field_with_errors');
+    });
+  };
 
-    var focusCommentsWithErrors = function() {
-      $(document).ready(function() {
-        if ($('.comment-form').hasClass('form_with_errors') === true) {
-          $('html, body').scrollTop($('.comment-form').offset().top);
-        }
-      });
-    };
+  // SCROLLS TO THE COMMENT-FORM IF COMMENT SUBMIT FAILED (TO SHOW THE ERROR MESSAGES TO THE USER)
+  var focusCommentsWithErrors = function() {
+    $(document).ready(function() {
+      if ($('.comment-form').hasClass('form_with_errors') === true) {
+        $('html, body').scrollTop($('.comment-form').offset().top);
+      }
+    });
+  };
 
-    var wrapTables = function() {
-        $('.content-formatted table').wrap('<div class="table-container overthrow"></div>');
-    };
+  // TODO: Remove if Edicy is going to wrap table with the container
+  var wrapTables = function() {
+    $('.content-formatted table').wrap('<div class="table-container overthrow"></div>');
+  };
 
-    var checkScrollBar = function() {
-        jQuery.fn.hasScrollBar = function(direction) {
-            if (direction == 'vertical') {
-                return this.get(0).scrollHeight > this.innerHeight();
-            } else if (direction == 'horizontal') {
-                return this.get(0).scrollWidth > this.innerWidth();
-            }
-            return false;
-        }
-    };
+  // TODO: Add comment
+  var checkScrollBar = function() {
+    jQuery.fn.hasScrollBar = function(direction) {
+      if (direction == 'vertical') {
+        return this.get(0).scrollHeight > this.innerHeight();
+      } else if (direction == 'horizontal') {
+        return this.get(0).scrollWidth > this.innerWidth();
+      }
+      return false;
+    }
+  };
 
-    var handleTableHorizontalScrolling = function() {
-        $.each($('.table-container'), function() {
-            if ($(this).hasScrollBar('horizontal') === true) {
-                $(this).addClass('horizontal-scroll');
-            } else {
-                $(this).removeClass('horizontal-scroll');
-            }
-        });
-    };
+  // ADDS HORIZONTAL SCROLL TO TABLES THAT DON'T FIT INTO THE CONTENT AREA
+  var handleTableHorizontalScrolling = function() {
+    $.each($('.table-container'), function() {
+      if ($(this).hasScrollBar('horizontal') === true) {
+        $(this).addClass('horizontal-scroll');
+      } else {
+        $(this).removeClass('horizontal-scroll');
+      }
+    });
+  };
 
-    var handleWindowResize = function() {
-        $(window).resize(function() {
-            handleTableHorizontalScrolling();
-        });
-    };
+  // INITIATES THE TABLE HORISONTAL SCROLL FUNCTION WHEN WINDOW IS RESIZED
+  var handleWindowResize = function() {
+    $(window).resize(function() {
+      handleTableHorizontalScrolling();
+    });
+  };
 
     // Initiations
     var initBlogPage = function() {
+      // ADD BLOG LISTING VIEW SPECIFIC FUNCTIONS HERE
     };
 
     var initArticlePage = function() {
-        handleFormFieldClick();
-        focusCommentsWithErrors();
+      // ADD SINGLE POST VIEW SPECIFIC FUNCTIONS HERE
+      handleFormFieldClick();
+      focusCommentsWithErrors();
+    };
+
+    var initCommonPage = function() {
+      // ADD COMMON PAGE SPECIFIC FUNCTIONS HERE
+      handleFormFieldClick();
+      focusCommentsWithErrors();
     };
 
     var init = function() {
-        toggleMainMenu();
-        toggleLangMenu();
-        handlePopoverMenuHide();
-        handleWindowResize();
-        wrapTables();
-        if ($('.table-container').length > 0) {
-            checkScrollBar();
-            handleTableHorizontalScrolling();
-        }
+      // ADD SITE WIDE FUNCTIONS HERE
+      handleLanguageSwitch();
+      toggleMainMenu();
+      handlePopoverMenuHide();
+      handleWindowResize();
+      wrapTables();
+      if ($('.table-container').length > 0) {
+        checkScrollBar();
+        handleTableHorizontalScrolling();
+      }
     };
 
+    // ENABLES THE USAGE OF THE INITIATIONS OUTSIDE THIS FILE
     window.site = $.extend(window.site || {}, {
-        initBlogPage: initBlogPage,
-        initArticlePage: initArticlePage,
-        handleFormFieldClick: handleFormFieldClick
+      initBlogPage: initBlogPage,
+      initArticlePage: initArticlePage,
+      initCommonPage: initCommonPage,
+      handleFormFieldClick: handleFormFieldClick
     });
 
     init();
-})(jQuery);
+  })(jQuery);
