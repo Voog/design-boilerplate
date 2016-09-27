f7245e510fc41ffe43637838265c7f3d43cef317
Kaspar Naaber <kaspar.naaber@outlook.com>
2016-09-27 11:14:09 +0300

    Update jQuery and Grunt build modules.

--- sources/javascripts/concat/main.js
+++ sources/javascripts/concat/main.js
@@ -127,7 +127,7 @@
 
       } else {
         colorExtractImage.attr('src', colorExtractImageUrl.replace(/.*\/(photos|voogstock)/g,'/photos'));
-        colorExtractImage.load(function() {
+        colorExtractImage.on('load', function() {
           ColorExtract.extract(colorExtractImage[0], colorExtractCanvas[0], function(data) {
             bgPicker.imageColor = data.bgColor ? data.bgColor : 'rgba(255,255,255,1)';
             bgPicker.combinedLightness = getCombinedLightness(bgPicker.imageColor, bgPickerColor);
